649b9b325ac0a93f9c53e98614cc46e70603fc7d
caoimhe daly <keve87@hotmail.com>
2018-10-25 15:59:57 +0000

    form completed

--- task_manager.py
+++ task_manager.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import os
 from flask_pymongo import PyMongo
 
@@ -14,6 +14,20 @@ mongo = PyMongo(app)
 def show_hi():
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks= tasks)
+    
+@app.route("/add_task", methods =["GET", "POST"])
+def add_task():
+    
+    if request.method=="POST":
+        mongo.db.tasks.insert_one(request.form.to_dict()) 
+        # this line converts our form to dictionary form and inserts it to mongo
+        return redirect("/")
+        
+        
+    
+    else:
+        categories = mongo.db.categories.find()
+        return render_template("add_task.html", categories=categories)
 
 if __name__ == "__main__":
         app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
